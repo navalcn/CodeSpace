1c41dcf44a8f114afc659cb74d875ea5a9388fd9
navalcn <navalcn4002@gmail.com>
2026-09-23 18:53:36 +0530

    Added Apis in sandbox server for user project creation, fetch all projs. and middlewares for auth

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,36 +1,25 @@
-import express from 'express'
-import morgan from 'morgan'
-import { createPod } from './kubernetes/pod.js'
-import { createService } from './kubernetes/service.js'
-import { v7 as uuid } from 'uuid'
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import sandboxRouter from './routes/sandbox.routes.js';
 
-const app = express()
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/api/sandbox/health', (req, res) => {
     res.status(200).json({
         message: 'Sandbox API is healthy',
         status: 'ok'
-    })
-})
+    });
+});
 
-app.post('/api/sandbox/start', async (req, res) => {
+app.use('/api/sandbox', sandboxRouter);
 
-    const sandboxId = uuid();
 
-    await Promise.all([
-        createPod(sandboxId),
-        createService(sandboxId)
-    ]);
-
-    return res.status(200).json({
-        message: 'Sandbox environment created successfully',
-        sandboxId,
-        previewUrl: `http://${sandboxId}.preview.localhost`
-    })
-})
 
 export default app;
